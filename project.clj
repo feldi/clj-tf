b2b397ab13dad8dc9ecca71d9040851844bf863e
feldi <peter.feldtmann@gmail.com>
2017-04-19 15:03:45 +0200

    bump project version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-tf "0.1.4"
+(defproject clj-tf "0.1.5"
   :description "Using Tensorflow with Clojure"
   :url "https://github.com/feldi/clj-tf"
   :license {:name "Eclipse Public License"
